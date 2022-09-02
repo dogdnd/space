e53c488af962e85eee4a6bd609a1413468142f22
dogdnd <dogdnd@gmail.com>
2022-09-02 11:38:51 +0900

    main

--- space_ml.py
+++ space_ml.py
@@ -19,14 +19,19 @@ import joblib
 def run_ml() :
 
     st.subheader('ML을 활용한 우주사업 예측모델')
+
+    
     df = pd.read_csv('data/space_mission.csv', thousands= ',')
  
 
+    st.info('우주사업 예산을 선택하세요')
+    rocket = st.slider('단위 $1M(=million)', min_value=100, max_value=10000,step= 100,)
+    st.info('발사시기를 선택하세요')
+    month= st.select_slider('월',options=[1,2,3,4,5,6,7,8,9,10,11,12])
 
-    rocket = st.slider('budget', min_value=100000, max_value=10000000,step= 10000,)
-    month= st.select_slider('month',options=[1,2,3,4,5,6,7,8,9,10,11,12])
-    weekday=st.select_slider('weekday',options= [0,1,2,3,4,5,6])
-    hour=st.slider('hour',1,24)
+    weekday=st.select_slider('요일',options= [0,1,2,3,4,5,6])
+
+    hour=st.slider('시간',1,24)
     
 
     classifier = joblib.load('data/classifier.pkl')
@@ -40,7 +45,9 @@ def run_ml() :
     data_pred = classifier.predict(scaled_data)
     data_pred = encoder.inverse_transform(data_pred)
 
-    st.write('this space mission is expected {}'.format(data_pred))
+    st.info('해당 조건에 따른 예측결과 확인')
+    if st.button('예측결과 확인') :
+        st.write('이 조건에 해당하는 우주사업은 {}으로 예측됩니다.'.format(data_pred))
 
 
 
